0a9b3e5c1286fa82c8a45b79692101177ac3e4f2
petraalpigiano <petraalpigiano@gmail.com>
2025-04-08 17:40:57 +0200

    solve 4°exercise

--- snack-funzioni/04_conta_vocali/script.js
+++ snack-funzioni/04_conta_vocali/script.js
@@ -1,13 +1,31 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
+/**
+ *
+ * @param {string} chosenWord parola/e
+ * @returns {number} numero delle vocali presenti nella parola/E
+ */
+function wordVowels(chosenWord) {
+  let howManyVowels = 0;
+  for (let i = 0; i < chosenWord.length; i++) {
+    const currentChar = chosenWord[i];
+    if (
+      currentChar === "a" ||
+      currentChar === "e" ||
+      currentChar === "i" ||
+      currentChar === "o" ||
+      currentChar === "u"
+    ) {
+      howManyVowels += 1;
+    }
+  }
 
-
+  return howManyVowels;
+}
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(wordVowels(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
