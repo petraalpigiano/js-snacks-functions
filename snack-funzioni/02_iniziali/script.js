886b8a7fc7845788e019136571f899f3077d4f8a
petraalpigiano <petraalpigiano@gmail.com>
2025-04-08 16:05:27 +0200

    solve 2°exercise

--- snack-funzioni/02_iniziali/script.js
+++ snack-funzioni/02_iniziali/script.js
@@ -1,13 +1,23 @@
-/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+/* Scrivi una funzione che accetti un array di nomi e restituisca di nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-
-
+const firstLetterName = [];
 // Dichiara la funzione qui.
 
+/**
+ *
+ * @param {Array} listNames lista di nomi
+ * @returns {Array} nuovo array con iniziali dei nomi dell'altro array
+ */
+function ListNames(listNames) {
+  for (let i = 0; i < listNames.length; i++) {
+    const currentName = listNames[i];
+    firstLetterName.push(currentName[0]);
+  }
+  return firstLetterName;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(ListNames(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
