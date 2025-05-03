fc0bd738fec8b1d05a41142d4421d46e6bebf81a
petraalpigiano <petraalpigiano@gmail.com>
2025-05-03 11:45:20 +0200

    solve 5°exercise

--- snack-funzioni/05_bonus/script.js
+++ snack-funzioni/05_bonus/script.js
@@ -5,14 +5,21 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
-
+const namee = "Mario";
 
 // Dichiara la funzione qui.
-
+function greeting(word) {
+  const currentHour = new Date().getHours();
+  if (currentHour <= 13) {
+    return `Buongiorno ${word}`;
+  } else if (currentHour <= 17) {
+    return `Buonpomeriggio ${word}`;
+  } else {
+    return `Buonasera ${word}`;
+  }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(greeting(namee));
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
